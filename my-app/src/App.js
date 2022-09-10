8d4c4ab89e2f75a4fd0bf91965f673e9f543e2f5
Ibrahim Younas <107543373+I-Younas@users.noreply.github.com>
2022-09-10 12:57:09 -0400

    Information Boxes for Company Information (#2)
    * Information Boxes for address,contact no, etc
    
    * Info boxes files

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./index.css"
+import Info from "./Info"
+
+function App() {
+  return (
+    <div>
+
+      <div>
+        <Info /> {/* Stories Screen */} 
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
